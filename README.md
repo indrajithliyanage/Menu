0dbbc3c13d836e2d108e80ddbe5fc6775f104287
indrajithliyanage <indrajith.udagama@outlook.com>
2023-01-08 19:36:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Menu filtering using Vanilla JS
---
+++ app.js
@@ -0,0 +1,132 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+  const categories = menu.reduce((values, items) => { 
+    if(!values.includes(items.category)){
+      values.push(items.category);
+    }
+    return values;
+  }, ['all']);
+
+  const categoryBtns = categories.map((category)=>{
+    return `<button class="filter-btn" type="button" data-cat="${category}">${category}</button>`
+  }).join("");
+
+  const container = document.querySelector('.btn-container');
+  container.innerHTML = categoryBtns;
+
+  const filterBtns = document.querySelectorAll('.filter-btn');
+  displayButtons(filterBtns);
+  displayItems(menu);  
+})
+
+function displayItems(filteredMenu) {
+  let section = document.querySelector('.section-center');
+  section.innerHTML = '';
+  filteredMenu.forEach((item, index) => {
+    const cardItem = `<!-- single item -->
+  <article class="menu-item ${index}">
+    <img src="${item.img}" alt="menu item ${index}" class="photo">
+    <div class="item-info">
+      <header>
+        <h4 class="name">${item.title}</h4>
+        <h4 class="price">${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>
+  <!-- end single item -->`
+    section.innerHTML += cardItem;
+  });
+}
+
+
+function displayButtons(filterBtns){
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.cat;
+      if (category == 'all') {        
+        displayItems(menu);
+      } else {
+      const filteredMenu = menu.filter(menuItems => category == menuItems.category)
+        displayItems(filteredMenu);
+      }
+    })
+  })
+}
+
